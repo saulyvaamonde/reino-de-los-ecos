8c3b9f2d65a877284c5f075864e55464000dc858
Sauly Vaamonde <116473228+saulyvaamonde@users.noreply.github.com>
2026-07-13 13:12:27 +0200

    Add files via upload
    Primer commit - Reino de los Ecos

---
+++ js/script.js
@@ -0,0 +1,74 @@
+const frases = [
+
+    "Las antiguas leyendas aún susurran entre los árboles...",
+    
+    "Cada sendero guarda un recuerdo olvidado.",
+    
+    "La magia nunca desapareció... solo esperaba ser encontrada.",
+    
+    "Todo reino necesita a alguien que escuche sus ecos.",
+    
+    "Los espíritus del bosque siempre encuentran a quien tiene un corazón noble.",
+    
+    "Más allá de las montañas, un reino espera despertar.",
+    
+    "No todos los héroes empuñan una espada; algunos devuelven la esperanza.",
+    
+    "Cada flor que vuelve a florecer cuenta una historia.",
+    
+    "Explora despacio. El Reino de los Ecos recompensa a los curiosos.",
+    
+    "El viento aún recuerda los nombres de los antiguos guardianes."
+    
+    ];
+    
+    const frase = document.getElementById("frase");
+    
+    let indice = 0;
+    
+    setInterval(() => {
+    
+    frase.classList.add("ocultar");
+    
+    setTimeout(() => {
+    
+    indice++;
+    
+    if(indice >= frases.length){
+    
+    indice = 0;
+    
+    }
+    
+    frase.textContent = `"${frases[indice]}"`;
+    
+    frase.classList.remove("ocultar");
+    
+    frase.classList.add("mostrar");
+    
+    },800);
+    
+    },5000);
+
+    const musica = document.getElementById("musica");
+    const boton = document.getElementById("btnConoce");
+
+    boton.addEventListener("click", () => {
+        musica.volume = 0.25;
+        musica.play();
+    });
+
+
+    const hero = document.querySelector(".hero");
+const fondo = document.querySelector(".parallax-layer");
+
+hero.addEventListener("mousemove", (e)=>{
+
+const x = (e.clientX / window.innerWidth - 0.5) * 20;
+
+const y = (e.clientY / window.innerHeight - 0.5) * 20;
+
+fondo.style.transform =
+`translate(${x}px, ${y}px) scale(1.08)`;
+
+});
