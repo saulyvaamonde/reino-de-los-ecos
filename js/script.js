dff746763c7f60cddba8cd7141bf43f07a7f9e04
Sauly Vaamonde <116473228+saulyvaamonde@users.noreply.github.com>
2026-07-22 19:40:25 +0200

    js cookies

--- js/script.js
+++ js/script.js
@@ -126,6 +126,20 @@ setInterval(createLeaf, 800);
     }
   });
 
+  /*cookies*/
 
+  const banner = document.getElementById("cookie-banner");
+
+  if(localStorage.getItem("cookiesAccepted")){
+  banner.style.display="none";
+  }
+  
+  document.getElementById("acceptCookies").onclick=function(){
+  
+  localStorage.setItem("cookiesAccepted","true");
+  
+  banner.style.display="none";
+  
+  }
 
   
